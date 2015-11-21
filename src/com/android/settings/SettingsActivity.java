cc18acb8ce4747f25dc3172fee8c31b824f36bb9
kufikugel <kufikugel@googlemail.com>
2015-11-21 07:09:08 +0000

    Configurable 0, 90, 180 and 270 degree rotation [2/2]
    PureNexus Edits:
    *Add shortcut to launch display rotation settings externally
    *Forward port to Nougat
    
    Change-Id: Ib0b191264d8923c5c46fec8054588c495a10b586

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -101,6 +101,7 @@ import com.android.settings.inputmethod.SpellCheckersSettings;
 import com.android.settings.inputmethod.UserDictionaryList;
 import com.android.settings.localepicker.LocaleListEditor;
 import com.android.settings.location.LocationSettings;
+import com.android.settings.aosip.DisplayRotation;
 import com.android.settings.nfc.AndroidBeam;
 import com.android.settings.nfc.PaymentSettings;
 import com.android.settings.notification.AppNotificationSettings;
@@ -394,7 +395,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             NavbarSettings.class.getName(),
             FlingSettings.class.getName(),
             SmartbarSettings.class.getName(),
-            PulseSettings.class.getName()
+            PulseSettings.class.getName(),
+            DisplayRotation.class.getName()
     };
 
 
