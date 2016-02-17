1d26b137faf1b61e02103d4bba6a3196831bd7b5
eyosen <abittin@gmail.com>
2016-02-17 11:02:14 +0530

    Fix systemUI restart command
    Change-Id: I0922ccfc91fcaf2a795f8fab1624f9dd5756d231

--- src/com/android/settings/utils/Helpers.java
+++ src/com/android/settings/utils/Helpers.java
@@ -27,7 +27,7 @@ public class Helpers {
     }
 
     public static void restartSystemUI() {
-        CMDProcessor.startSuCommand("pkill -TERM -f com.android.systemui");
+        CMDProcessor.startSuCommand("pkill -f com.android.systemui");
     }
 }
 
