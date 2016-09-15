7cb155855f761059bd3d1ee4ee5b26e699343411
Akhil Narang <akhil@akhilnarang.me>
2016-09-15 19:16:57 +0530

    Settings: build in OwlsNest
    Change-Id: I6f73ff01228f2c44df02b545f49a69a821afd3f7
    Signed-off-by: Akhil Narang <akhil@akhilnarang.me>

--- Android.mk
+++ Android.mk
@@ -27,9 +27,11 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := \
         $(call all-java-files-under, src) \
-        src/com/android/settings/EventLogTags.logtags
+        src/com/android/settings/EventLogTags.logtags \
+        $(call all-java-files-under, ../OwlsNest/src)
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
+	$(LOCAL_PATH)/../OwlsNest/res \
     frameworks/support/v7/preference/res \
     frameworks/support/v14/preference/res \
     frameworks/support/v7/appcompat/res \
@@ -42,7 +44,7 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_AAPT_FLAGS := --auto-add-overlay \
-    --extra-packages android.support.v7.preference:android.support.v14.preference:android.support.v17.preference:android.support.v7.appcompat:android.support.v7.recyclerview
+    --extra-packages android.support.v7.preference:android.support.v14.preference:android.support.v17.preference:android.support.v7.appcompat:android.support.v7.recyclerview:com.aosip.owlsnest
 
 ifneq ($(INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled
