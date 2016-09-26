813e096c4bc1e61b847eb9969b17cd3594fd851d
beanstown106 <nbenis106@gmail.com>
2016-09-25 21:28:13 -0400

    Add Phh Superuser dashboard tile credit to tsubus for his setting dashboard implementation
    Change-Id: I6e3992f13b57bc74d3dbb6e9afb332fa93449a6a

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -168,4 +168,5 @@ public class Settings extends SettingsActivity {
     public static class SuperSUActivity extends SettingsActivity { /* empty */ }
     public static class STweaksActivity extends SettingsActivity { /* empty */ }
     public static class ToolboxActivity extends SettingsActivity { /* empty */ }
+    public static class SuperUserActivity extends SettingsActivity { /* empty */ }
 }
