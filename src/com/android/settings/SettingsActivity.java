xlxfoxxlx/platform_packages_apps_Settings-1

Integrate OwlsNest into Settings
Thanks to @camcory for the manifest part, big help there

Change-Id: Id4e5367bab5826bfc8bced5684fc2e599f8140c4

src/com/android/settings/SettingsActivity.java
@@ -128,6 +128,8 @@ import com.android.settingslib.drawer.DashboardCategory;
 import com.android.settingslib.drawer.SettingsDrawerActivity;
 import com.android.settingslib.drawer.Tile;
 
+import com.aosip.owlsnest.OwlsNestSettings;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -345,6 +347,7 @@ public class SettingsActivity extends SettingsDrawerActivity
             TestingSettings.class.getName(),
             WifiAPITest.class.getName(),
             WifiInfo.class.getName(),
+            OwlsNestSettings.class.getName(),
     };
 
 

AOSiP/platform_packages_apps_Settings

src/com/android/settings/SettingsActivity.java
@@ -138,6 +138,8 @@ import com.android.settingslib.drawer.SettingsDrawerActivity;
 import com.android.settingslib.drawer.Tile;
 
 import java.net.URISyntaxException;
+import com.aosip.owlsnest.OwlsNestSettings;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -383,7 +385,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             NightDisplaySettings.class.getName(),
             ManageDomainUrls.class.getName(),
             AutomaticStorageManagerSettings.class.getName(),
-            ExpandedDesktopPreferenceFragment.class.getName()
+            ExpandedDesktopPreferenceFragment.class.getName(),
+            OwlsNestSettings.class.getName()
     };
 
 