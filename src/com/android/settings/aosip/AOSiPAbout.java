3427b52ebfae6c46ea309db72c7b5385e1b42295
Akhil Narang <akhilnarang.1999@gmail.com>
2017-07-12 21:27:14 +0200

    Settings: cleanup
      * Aosip -> AOSiP
      * Rearrange About Phone a bit
      * AOSiP Build -> AOSiP Version
      * Add Telegram chat link in About
    
    Change-Id: I8c72075f68c258cab7d60b18b83cb14c237b89de
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- src/com/android/settings/aosip/AOSiPAbout.java
+++ src/com/android/settings/aosip/AOSiPAbout.java
@@ -7,14 +7,14 @@ import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class AosipAbout extends SettingsPreferenceFragment {
+public class AOSiPAbout extends SettingsPreferenceFragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.aosip_about_info);
-	getActivity().getActionBar().setTitle("About AOSiP");
+    getActivity().getActionBar().setTitle("About AOSiP");
     }
 
     @Override
