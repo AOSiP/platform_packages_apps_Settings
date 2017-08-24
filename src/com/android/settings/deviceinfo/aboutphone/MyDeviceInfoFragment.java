97d8bd804b398b6de7e8f74cfe7c80953b685ff1
Harsh Shandilya <msfjarvis@gmail.com>
2017-08-24 13:11:31 +0000

    Settings: Add AOSiP version to "About Phone".
    Change-Id: Ie875701d39e88d27ddc3565e9fd0cf7f475d5639
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
+++ src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
@@ -29,6 +29,7 @@ import android.view.View;
 import com.android.settings.R;
 import com.android.settings.Utils;
 import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.deviceinfo.AOSIPVersionPreferenceController;
 import com.android.settings.deviceinfo.BluetoothAddressPreferenceController;
 import com.android.settings.deviceinfo.BuildNumberPreferenceController;
 import com.android.settings.deviceinfo.DeviceNamePreferenceController;
@@ -117,6 +118,7 @@ public class MyDeviceInfoFragment extends DashboardFragment
         controllers.add(new FccEquipmentIdPreferenceController(context));
         controllers.add(new UptimePreferenceController(context, lifecycle));
         controllers.add(new SELinuxStatusPreferenceController(context));
+        controllers.add(new AOSIPVersionPreferenceController(context));
         return controllers;
     }
 
