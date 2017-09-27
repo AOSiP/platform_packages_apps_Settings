d5ac083af0bdef676e61b146f88295d810703ba3
Harry Youd <harry@harryyoud.co.uk>
2017-09-27 19:48:42 +0100

    Settings: Add Updater
    Adding to both "System" (top category), and "About phone"
    (subcategory of "System"), similar to that of the GMS System Update.
    
    AOSiP Edits:
     - Removed from "About Phone"
     - Fixed for 10
    
    Change-Id: Icca0f96720d0851738490ad543b54ec82cb19fa2
    Signed-off-by: Brett Montague <bmontagu@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
    Signed-off-by: xlxfoxxlx <joshfox87@gmail.com>
    Signed-off-by: argraur <argraur@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
+++ src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
@@ -43,6 +43,7 @@ import com.android.settings.deviceinfo.UptimePreferenceController;
 import com.android.settings.deviceinfo.WifiMacAddressPreferenceController;
 import com.android.settings.deviceinfo.imei.ImeiInfoPreferenceController;
 import com.android.settings.deviceinfo.simstatus.SimStatusPreferenceController;
+import com.android.settings.deviceinfo.UpdatePreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.widget.EntityHeaderController;
 import com.android.settingslib.core.AbstractPreferenceController;
@@ -117,6 +118,7 @@ public class MyDeviceInfoFragment extends DashboardFragment
         controllers.add(new FccEquipmentIdPreferenceController(context));
         controllers.add(new UptimePreferenceController(context, lifecycle));
         controllers.add(new SELinuxStatusPreferenceController(context));
+	controllers.add(new UpdatePreferenceController(context));
         return controllers;
     }
 
