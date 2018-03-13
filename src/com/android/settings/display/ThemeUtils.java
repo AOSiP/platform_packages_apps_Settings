AOSiP/platform_packages_apps_Settings

commit e2b822dd894487cd1971ca7049286a4ca9d63ab1

Settings: Disable system theme preferences when Substratum overlays are enabled
Rather than unconditionally disabling system themes when Substratum is detected,
only do so when there is a Substratum theme applied to the framework.

Improves on a (not so) similar commit by Alex Cruz.

Change-Id: If1b601a7184c96fc33347da2cfff5ac2f2d7aca7
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

src/com/android/settings/display/ThemeUtils.java
@@ -1,0 +1,79 @@
+/*
+ * Copyright (c) 2018 AOSiP
+ * Contributed by Projekt Substratum
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+
+ */
+
+package com.android.settings.display;
+
+import android.content.Context;
+import android.content.om.IOverlayManager;
+import android.content.om.OverlayInfo;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.UserHandle;
+
+import java.util.List;
+
+
+public class ThemeUtils {
+
+    private static final String substratumVersionMetadata = "Substratum_Version";
+
+    private static boolean isSubstratumOverlay(
+            Context mContext,
+            String packageName) {
+        try {
+            ApplicationInfo appInfo = mContext.getPackageManager().getApplicationInfo(
+                    packageName, PackageManager.GET_META_DATA);
+            if (appInfo.metaData != null) {
+                int returnMetadata = appInfo.metaData.getInt(substratumVersionMetadata);
+                if (String.valueOf(returnMetadata) != null) {
+                    return true;
+                }
+            }
+        } catch (Exception ignored) {
+        }
+        return false;
+    }
+
+    public static boolean isSubstratumOverlayInstalled(Context mContext) {
+        try {
+            OverlayManager mOverlayManager = new OverlayManager();
+            List<OverlayInfo> overlayInfoList =
+                    mOverlayManager.getOverlayInfosForFramework();
+            for (int i = 0; i < overlayInfoList.size(); i++) {
+                if (isSubstratumOverlay(mContext, overlayInfoList.get(i).packageName))
+                    return true;
+            }
+        } catch (RemoteException ignored) {
+        }
+        return false;
+    }
+
+    static class OverlayManager {
+        private final IOverlayManager mService;
+
+        OverlayManager() {
+            mService = IOverlayManager.Stub.asInterface(
+                    ServiceManager.getService(Context.OVERLAY_SERVICE));
+        }
+
+        List<OverlayInfo> getOverlayInfosForFramework()
+                throws RemoteException {
+            return mService.getOverlayInfosForTarget("android", UserHandle.myUserId());
+        }
+    }
+}

res/values/aosip_strings.xml
@@ -264,4 +264,5 @@
 
     <string name="applying_theme_toast">Applying theme, please wait...</string>
     <string name="theme_applied_toast">Theme applied!</string>
+    <string name="substratum_overlays_installed_title">System themes are unavailable because a Substratum overlay is installed. Please uninstall all overlays targeting Android System to proceed.</string>
 </resources>

src/com/android/settings/display/AccentPickerPreferenceController.java
@@ -17,12 +17,12 @@ package com.android.settings.display;
 
 import android.app.Fragment;
 import android.content.Context;
-import android.os.UserHandle;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceClickListener;
 import android.support.v7.preference.PreferenceScreen;
 
 import com.android.settings.core.PreferenceControllerMixin;
+import static com.android.settings.display.ThemeUtils.isSubstratumOverlayInstalled;
 
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
@@ -35,7 +35,6 @@ public class AccentPickerPreferenceController extends AbstractPreferenceControll
         implements PreferenceControllerMixin, LifecycleObserver, OnResume {
 
     private static final String KEY_ACCENT_PICKER_FRAGMENT_PREF = "accent_picker";
-    private static final int MY_USER_ID = UserHandle.myUserId();
 
     private final Fragment mParent;
     private Preference mAccentPickerPref;
@@ -51,6 +50,8 @@ public class AccentPickerPreferenceController extends AbstractPreferenceControll
     @Override
     public void displayPreference(PreferenceScreen screen) {
         mAccentPickerPref  = (Preference) screen.findPreference(KEY_ACCENT_PICKER_FRAGMENT_PREF);
+        if (isSubstratumOverlayInstalled(mContext))
+            mAccentPickerPref.setEnabled(false);
     }
 
     @Override
@@ -78,17 +79,25 @@ public class AccentPickerPreferenceController extends AbstractPreferenceControll
             new OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
-                    AccentPicker.show(mParent);
-                    return true;
+                   if (!isSubstratumOverlayInstalled(mContext)) {
+                        AccentPicker.show(mParent);
+                        return true;
+                   } else {
+                        return false;
+                   }
                 }
             });
     }
 
     public void updateSummary() {
         if (mAccentPickerPref != null) {
+            if (!isSubstratumOverlayInstalled(mContext)) {
                 mAccentPickerPref.setSummary(mContext.getString(
                         com.android.settings.R.string.theme_accent_picker_summary));
+            } else {
+                mAccentPickerPref.setSummary(mContext.getString(
+                        com.android.settings.R.string.substratum_installed_title));
+            }
         }
     }
-
 }

src/com/android/settings/display/DarkUIPreferenceController.java
@@ -22,6 +22,8 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
 import android.provider.Settings;
 
+import static com.android.settings.display.ThemeUtils.isSubstratumOverlayInstalled;
+
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.drawer.SettingsDrawerActivity;
@@ -59,12 +61,17 @@ public class DarkUIPreferenceController extends AbstractPreferenceController imp
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
         mSystemUiThemeStyle = (ListPreference) screen.findPreference(SYSTEM_UI_THEME);
-        int systemuiThemeStyle = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.SYSTEM_UI_THEME, 0);
-        int valueIndex = mSystemUiThemeStyle.findIndexOfValue(String.valueOf(systemuiThemeStyle));
-        mSystemUiThemeStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
-        mSystemUiThemeStyle.setSummary(mSystemUiThemeStyle.getEntry());
-        mSystemUiThemeStyle.setOnPreferenceChangeListener(this);
+        if (!isSubstratumOverlayInstalled(mContext)) {
+            int systemuiThemeStyle = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.SYSTEM_UI_THEME, 0);
+            int valueIndex = mSystemUiThemeStyle.findIndexOfValue(String.valueOf(systemuiThemeStyle));
+            mSystemUiThemeStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
+            mSystemUiThemeStyle.setSummary(mSystemUiThemeStyle.getEntry());
+            mSystemUiThemeStyle.setOnPreferenceChangeListener(this);
+        } else {
+            mSystemUiThemeStyle.setEnabled(false);
+            mSystemUiThemeStyle.setSummary(R.string.substratum_installed_title);
+        }
     }
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {

src/com/android/settings/display/ThemePreferenceController.java
@@ -149,9 +149,10 @@ public class ThemePreferenceController extends AbstractPreferenceController impl
 
     @Override
     public boolean isAvailable() {
-        if (mOverlayService == null) return false;
-        String[] themes = getAvailableThemes();
-        return themes != null && themes.length > 1;
+        //if (mOverlayService == null) return false;
+        //String[] themes = getAvailableThemes();
+        //return themes != null && themes.length > 1;
+        return false;
     }
 
 

commit 11da869dfa45b96eec53d9838f2152c77842abd8

ThemeUtils: Cleanup and account for RROs
Devices like mata, walleye and taimen ship a android.auto_generated_rro__
overlay in their vendor images, these are not to be disabled
and they don't break UI elements either, so bypass them from our
substratum overlay check.

Change-Id: I1eb9aafd1f170b0c498ad8c9470209bb76a2d0c0
Reported-by: Josh Fox <joshfox87@gmail.com>
Fixes: e2b822dd8944 ("Settings: Disable system theme preferences when Substratum overlays are enabled")
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

src/com/android/settings/display/ThemeUtils.java
@@ -30,7 +30,8 @@ import java.util.List;
 
 public class ThemeUtils {
 
-    private static final String substratumVersionMetadata = "Substratum_Version";
+    private static final String SUBSTRATUM_VERSION_METADATA = "Substratum_Version";
+    private static final String[] WHITELISTED_OVERLAYS = { "android.auto_generated_rro__" };
 
     private static boolean isSubstratumOverlay(
             Context mContext,
@@ -39,7 +40,7 @@ public class ThemeUtils {
             ApplicationInfo appInfo = mContext.getPackageManager().getApplicationInfo(
                     packageName, PackageManager.GET_META_DATA);
             if (appInfo.metaData != null) {
-                int returnMetadata = appInfo.metaData.getInt(substratumVersionMetadata);
+                int returnMetadata = appInfo.metaData.getInt(SUBSTRATUM_VERSION_METADATA);
                 if (String.valueOf(returnMetadata) != null) {
                     return true;
                 }
@@ -54,8 +55,9 @@ public class ThemeUtils {
             OverlayManager mOverlayManager = new OverlayManager();
             List<OverlayInfo> overlayInfoList =
                     mOverlayManager.getOverlayInfosForFramework();
-            for (int i = 0; i < overlayInfoList.size(); i++) {
-                if (isSubstratumOverlay(mContext, overlayInfoList.get(i).packageName))
+            for (OverlayInfo overlay : overlayInfoList) {
+                if (WHITELISTED_OVERLAYS.contains(overlay.packageName)) continue;
+                if (isSubstratumOverlay(mContext, overlay.packageName))
                     return true;
             }
         } catch (RemoteException ignored) {