bdd142c4065af22af9bbf8246c0ab0f2d1ca0787
Chris <chris@cypheros.co>
2018-04-25 15:51:45 -0400

    deviceinfo: Add support for overriding vendor properties
    Some properties will forcefully be read from vendor instead of system
    which can result in outdated or misrepresented OEM information. As a
    small workaround, allow overriding the current props that are displayed
    in about phone.
    
    This way we can set our own props and display proper information such as
    the security patch level. Although this is minor, asthetics matter.
    Current supported props are
    
    "ro.vendor.override.security_patch"
    "ro.vendor.override.build_display"
    
    To override the device model, simply add this string overlay with the
    desired model "config_overridenVendorProductModel"
    
    This is a last option incase we can't override these traditionally
    
    Change-Id: I4b99bab6dc5249ac81827b10c8069c82ed43e968
    Signed-off-by: Jagrav Naik <jagravnaik0@gmail.com>

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -20,7 +20,9 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.provider.SearchIndexableResource;
+import android.text.TextUtils;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.dashboard.DashboardFragment;
@@ -44,6 +46,7 @@ import com.android.settings.deviceinfo.SecurityPatchPreferenceController;
 import com.android.settings.deviceinfo.VendorVersionPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
+import com.android.settingslib.DeviceInfoUtils;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
 
@@ -95,16 +98,28 @@ public class DeviceInfoSettings extends DashboardFragment implements Indexable {
 
     private static class SummaryProvider implements SummaryLoader.SummaryProvider {
 
+        private final Context mContext;
         private final SummaryLoader mSummaryLoader;
 
-        public SummaryProvider(SummaryLoader summaryLoader) {
+        public SummaryProvider(Context context, SummaryLoader summaryLoader) {
+            mContext = context;
             mSummaryLoader = summaryLoader;
+        }
+
+        public String getDeviceModel() {
+            String modelOverride = mContext.getResources().getString(
+                R.string.config_overridenVendorProductModel);
+            if (!TextUtils.isEmpty(modelOverride)) {
+                return modelOverride;
+            } else {
+                return Build.MODEL + DeviceInfoUtils.getMsvSuffix();
+            }
         }
 
         @Override
         public void setListening(boolean listening) {
             if (listening) {
-                mSummaryLoader.setSummary(this, DeviceModelPreferenceController.getDeviceModel());
+                mSummaryLoader.setSummary(this, getDeviceModel());
             }
         }
     }
@@ -114,7 +129,7 @@ public class DeviceInfoSettings extends DashboardFragment implements Indexable {
         @Override
         public SummaryLoader.SummaryProvider createSummaryProvider(Activity activity,
                 SummaryLoader summaryLoader) {
-            return new SummaryProvider(summaryLoader);
+            return new SummaryProvider(activity, summaryLoader);
         }
     };
 
