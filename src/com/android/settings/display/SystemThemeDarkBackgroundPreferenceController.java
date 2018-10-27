d41206a3ffd2bbe400336704f9ba8355442e2a4a
Akhil Narang <akhilnarang.1999@gmail.com>
2018-10-27 23:13:08 +0530

    ThemeStuff: cleanup black theme stuff
    Instead of adding it as an alternative background, we'll add it as a separate
    option when the dark theme is selected
    
    Change-Id: I9c460aaff47b69e9b7442f313b420eed91bc5e44
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

---
+++ src/com/android/settings/display/SystemThemeDarkBackgroundPreferenceController.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) 2018 AOSiP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.android.settings.display;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+import com.android.settings.R;
+import static com.android.settings.display.ThemeUtils.isSubstratumOverlayInstalled;
+
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.drawer.SettingsDrawerActivity;
+
+import libcore.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SystemThemeDarkBackgroundPreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
+
+    private static final String SYSTEM_THEME_DARK_BACKGROUND = "system_theme_dark_background_style";
+
+    private ListPreference mDarkBackgroundStyle;
+
+    public SystemThemeDarkBackgroundPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return SYSTEM_THEME_DARK_BACKGROUND;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mDarkBackgroundStyle = (ListPreference) screen.findPreference(SYSTEM_THEME_DARK_BACKGROUND);
+        boolean isUsingDarkTheme = Settings.System.getInt
+                (mContext.getContentResolver(), Settings.System.SYSTEM_THEME, 0);
+        if (isSubstratumOverlayInstalled(mContext)) {
+            mDarkBackgroundStyle.setEnabled(false);
+            mDarkBackgroundStyle.setSummary(R.string.substratum_overlays_installed_title);
+        } else if (!isUsingDarkTheme) {
+            mDarkBackgroundStyle.setEnabled(false);
+            mDarkBackgroundStyle.setSummary(R.string.dark_theme_not_chosen);
+        } else {
+            int darkBackgroundStyle = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.SYSTEM_THEME_DARK_BACKGROUND, 0);
+            int valueIndex = mDarkBackgroundStyle.findIndexOfValue(String.valueOf(darkBackgroundStyle));
+            mDarkBackgroundStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
+            mDarkBackgroundStyle.setSummary(mDarkBackgroundStyle.getEntry());
+            mDarkBackgroundStyle.setOnPreferenceChangeListener(this);
+        }
+    }
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mDarkBackgroundStyle) {
+            String value = (String) newValue;
+            Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.SYSTEM_THEME_DARK_BACKGROUND,
+                    Integer.valueOf(value));
+            int valueIndex = mDarkBackgroundStyle.findIndexOfValue(value);
+            mDarkBackgroundStyle.setSummary(mDarkBackgroundStyle.getEntries()[valueIndex]);
+            try {
+                reload();
+            }catch (Exception ignored){
+            }
+        }
+        return true;
+    }
+    private void reload(){
+        Intent intent2 = new Intent(Intent.ACTION_MAIN);
+        intent2.addCategory(Intent.CATEGORY_HOME);
+        intent2.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        mContext.startActivity(intent2);
+        Toast.makeText(mContext, R.string.applying_theme_toast, Toast.LENGTH_SHORT).show();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+              @Override
+              public void run() {
+                  Intent intent = new Intent(Intent.ACTION_MAIN);
+                  intent.setClassName("com.android.settings",
+                        "com.android.settings.Settings$DisplaySettingsActivity");
+                  intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                  intent.putExtra(SettingsDrawerActivity.EXTRA_SHOW_MENU, true);
+                  mContext.startActivity(intent);
+                  Toast.makeText(mContext, R.string.theme_applied_toast, Toast.LENGTH_SHORT).show();
+              }
+        }, 2000);
+    }
+}
