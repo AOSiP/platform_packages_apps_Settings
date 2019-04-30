492e26ce878f83fc63fd93f48234fef1f6675463
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2019-04-30 00:35:43 -0400

    Settings: Adjust to our support switch
    Change-Id: I90299b4ba02c083675c0d13cdd49fc13d1c286f6
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- src/com/android/settings/notification/CustomLightOnTimePreferenceController.java
+++ src/com/android/settings/notification/CustomLightOnTimePreferenceController.java
@@ -28,7 +28,7 @@ import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.core.lifecycle.LifecycleObserver;
 import com.android.settingslib.core.lifecycle.events.OnResume;
 
-import com.aosip.owlsnest.preference.CustomSeekBarPreference;
+import com.aosip.support.preference.CustomSeekBarPreference;
 
 public class CustomLightOnTimePreferenceController extends NotificationPreferenceController
         implements PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
