5c694bf5396134a4769b5facc707bb8f7001bf84
LorDClockaN <lordclockan@gmail.com>
2020-01-01 16:24:03 -0500

    Launch DU Themes from Settings
    Also:
    - Hide the dark switch from under Settings/Display
    * No need to revert everything that DU reverted, cause we didn't have it.
    * Taken from this commit: 5c81bac6bb95501f1a8b770e668f2c8a7671eb74
    
    * Scorpion edits:
    - Updated for Android 11
    
    Change-Id: I3cdcca0e352a92a8ddb60d021a79811e105d487e

---
+++ src/com/android/settings/StartThemes.java
@@ -0,0 +1,35 @@
+package com.android.settings;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import com.android.settings.R;
+import com.android.settings.dashboard.SummaryProvider;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.SettingsSearchIndexablesProvider;
+
+public class StartThemes extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startThemes();
+        finish();
+    }
+
+    private void startThemes(){
+        Intent themesExtrasIntent = new Intent();
+        themesExtrasIntent.setClassName(
+                "com.dirtyunicorns.themes", "com.dirtyunicorns.themes.MainActivity");
+        startActivity(themesExtrasIntent);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.OWLSNEST;
+    }
+}
