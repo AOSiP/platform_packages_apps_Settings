omnirom/android_packages_apps_Settings

[2/2] Settings: dont block IME space for back gesture
Change-Id: Iec0d223ecaebdae7123c3188f7963a83cf9be27d

src/com/android/settings/gestures/SystemNavigationGestureSettings.java
@@ -162,7 +162,7 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment {
             p.setExtraWidgetVisibility(EXTRA_WIDGET_VISIBILITY_SETTING);
             p.setExtraWidgetOnClickListener((v) -> GestureNavigationBackSensitivityDialog
                         .show(this, getBackSensitivity(getContext(), mOverlayManager),
-                        getBackHeight(getContext())));
+                        getBackHeight(getContext()), getBackBlockIme(getContext())));
         } else {
             p.setExtraWidgetVisibility(EXTRA_WIDGET_VISIBILITY_GONE);
         }
@@ -266,6 +266,16 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment {
     static int getBackHeight(Context context) {
         return Settings.System.getInt(context.getContentResolver(),
                 Settings.System.OMNI_BACK_GESTURE_HEIGHT, 0);
+    }
+
+    static void setBackBlockIme(Context context, boolean blockIme) {
+        Settings.System.putInt(context.getContentResolver(),
+                Settings.System.OMNI_BACK_GESTURE_BLOCK_IME, blockIme ? 1 : 0);
+    }
+
+    static boolean getBackBlockIme(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.OMNI_BACK_GESTURE_BLOCK_IME, 1) == 1;
     }
 
     @VisibleForTesting

AOSiP/platform_packages_apps_Settings

src/com/android/settings/gestures/SystemNavigationGestureSettings.java
@@ -162,7 +162,7 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment {
             p.setExtraWidgetVisibility(EXTRA_WIDGET_VISIBILITY_SETTING);
             p.setExtraWidgetOnClickListener((v) -> GestureNavigationBackSensitivityDialog
                     .show(this, getBackSensitivity(getContext(), mOverlayManager),
-                    getBackHeight(getContext())));
+                    getBackHeight(getContext()), getBackBlockIme(getContext())));
         } else {
             p.setExtraWidgetVisibility(EXTRA_WIDGET_VISIBILITY_GONE);
         }
@@ -266,6 +266,16 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment {
     static int getBackHeight(Context context) {
         return Settings.System.getInt(context.getContentResolver(),
                 Settings.System.BACK_GESTURE_HEIGHT, 0);
+    }
+
+    static void setBackBlockIme(Context context, boolean blockIme) {
+        Settings.System.putInt(context.getContentResolver(),
+                Settings.System.BACK_GESTURE_BLOCK_IME, blockIme ? 1 : 0);
+    }
+
+    static boolean getBackBlockIme(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.BACK_GESTURE_BLOCK_IME, 1) == 1;
     }
 
     @VisibleForTesting