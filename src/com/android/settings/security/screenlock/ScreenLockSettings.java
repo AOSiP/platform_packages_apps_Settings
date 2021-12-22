tronnayan/packages_apps_settings

Settings: Add option to scramble pin layout when unlocking [1/2]
@mydongistiny: Ported to Android Q
Change-Id: I3e2c200a0a31d3c765831bc30280029a50c88051
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

src/com/android/settings/security/screenlock/ScreenLockSettings.java
@@ -82,6 +82,8 @@ public class ScreenLockSettings extends DashboardFragment
         controllers.add(new LockAfterTimeoutPreferenceController(
                 context, MY_USER_ID, lockPatternUtils));
         controllers.add(new QuickUnlockPreferenceController(
+		context, MY_USER_ID, lockPatternUtils));
+        controllers.add(new ScramblePinPreferenceController(
                 context, MY_USER_ID, lockPatternUtils));
         controllers.add(new OwnerInfoPreferenceController(context, parent, lifecycle));
         return controllers;

AOSiP/platform_packages_apps_Settings

src/com/android/settings/security/screenlock/ScreenLockSettings.java
@@ -77,6 +77,8 @@ public class ScreenLockSettings extends DashboardFragment
                 context, MY_USER_ID, lockPatternUtils));
         controllers.add(new QuickUnlockPreferenceController(
                 context, MY_USER_ID, lockPatternUtils));
+        controllers.add(new PinScramblePreferenceController(
+                context, MY_USER_ID, lockPatternUtils));
         controllers.add(new OwnerInfoPreferenceController(context, parent));
         return controllers;
     }